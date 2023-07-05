27c54aa7e6038314c4cd36d13c15b8e7d8517681
oldweipro <oldweipro@gmail.com>
2023-07-05 23:22:27 +0800

    update

--- openDocument.js
+++ openDocument.js
@@ -1,13 +1,6 @@
-/*
-                    商用代码公司自用产品无需授权
-    若作为代码出售的产品（任何涉及代码交付第三方作为后续开发）必须保留此脚本
-                         或标注原作者信息
-                          否则将依法维权
-*/
-
 const child_process = require('child_process')
 
-const url = 'https://www.gin-vue-admin.com'
+const url = 'https://chat.oldwei.com'
 let cmd = ''
 console.log(process.platform)
 switch (process.platform) {
